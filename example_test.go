5951e2b81d7e7c31bdf72282f5aa41d8d9e88fbc
Dimitris Dranidis <dranidis@gmail.com>
2018-06-04 17:41:13 +0300

    keys

--- example_test.go
+++ example_test.go
@@ -56,16 +56,15 @@ func Example() {
 	go sdl.ChannelConsumer(die, "ENV", out)
 
 	sdl.Execute(
-		sdl.Transmission{10, counterChan, UP{}},
-		sdl.Transmission{10, counterChan, DN{}},
-		sdl.Transmission{10, counterChan, UP{4}},
-		sdl.Transmission{10, counterChan, UP{}},
-		sdl.Transmission{10, counterChan, DN{}},
-		sdl.Transmission{10, counterChan, DN{}},
-		sdl.Transmission{10, counterChan, DN{}},
-		sdl.Transmission{10, counterChan, DN{}},
-		sdl.Transmission{10, counterChan, DN{}},
-		sdl.Transmission{10, counterChan, UP{}},
+		sdl.Transmission{MsDelay:10, Receiver:counterChan, Signal:UP{}},
+		sdl.Transmission{MsDelay:10, Receiver:counterChan, Signal:DN{}},
+		sdl.Transmission{MsDelay:10, Receiver:counterChan, Signal:UP{4}},
+		sdl.Transmission{MsDelay:10, Receiver:counterChan, Signal:DN{}},
+		sdl.Transmission{MsDelay:10, Receiver:counterChan, Signal:DN{}},
+		sdl.Transmission{MsDelay:10, Receiver:counterChan, Signal:DN{}},
+		sdl.Transmission{MsDelay:10, Receiver:counterChan, Signal:DN{}},
+		sdl.Transmission{MsDelay:10, Receiver:counterChan, Signal:DN{}},
+		sdl.Transmission{MsDelay:10, Receiver:counterChan, Signal:UP{}},
 	)
 
 	time.Sleep(2000 * time.Millisecond)
